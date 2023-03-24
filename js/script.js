a0f4c9a00f2755b6cd0750e69a5979ff3cdad9ab
leonardochen01 <leonardochen.lc@gmail.com>
2023-03-24 18:14:07 +0100

    added: not properly working previous button

--- js/script.js
+++ js/script.js
@@ -11,14 +11,25 @@ const images = [
   document.getElementById('img5')
 ];
 
-//faccio la function eventlistener del secondo bottone
+//faccio la function eventlistener del secondo bottone ( next )
 const maxI = 5;
 let i = 1;
 buttonNext.addEventListener("click", function(){
 i = i >= maxI ? 1 : i + 1; //per questo e il maxI mi sono informato su internet
-console.log(i)
 
 //rimuovo classe show a tutti e lo aggiungo soltanto all'immagine corrispondente
 images.forEach(image => image.classList.remove("show"));
 images[i - 1].classList.add("show");
 })
+
+//inizio function del primo bottone ( prev )
+const minII = 1;
+let ii = 5;
+buttonPrev.addEventListener("click", function(){
+  
+  images.forEach(image => image.classList.remove("show"));
+  images[ii - 1].classList.add("show");
+  ii = ii <= minII ? 5 : ii - 1;
+  console.log(i)
+}) //PROBLEMA, ho fatto copia e incolla della function precedente e ho invertito i dati, apparentemente funziona ma nella console vedo che il counter non scende correttamente, comunque sia non penso di saperlo e volerlo modificare, anche perchè se funziona allora non si tocca
+//okay. non riesco a farlo funzionare, se clicco sia prev che next le immagini non scorrono bene, provero a inventarmi qualcosa
